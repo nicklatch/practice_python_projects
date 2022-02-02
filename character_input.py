38444a55230ef85a07212b78ea41c772b3c14abd
nicklatch <nick.lat.73@gmail.com>
2022-02-02 17:14:50 -0600

    moved variables inside function def.

--- character_input.py
+++ character_input.py
@@ -1,10 +1,12 @@
 # Character Input
-def char_in(name, age, year, bday):
+def char_in():
+    name = input("What is your name? ")
+    age = int(input("How old are you? "))
+    year = int(input("What year is it? "))
+    bday = input("Have you had your birthday yet this year? (Y/N) ")
     if bday.upper() == 'Y':
         return(name + " you will turn 100 in the year " + str((year - age) + 100))
     else:
         return(name + " you will turn 100 in the year " + str((year - age) + 99))
 
-
-print(char_in(input("What is your name? "),
-      (int(input("How old are you? "))), int(input("What year is it? ")), input("Have you had your birthday yet this year? (Y/N) ")))
+print(char_in())
