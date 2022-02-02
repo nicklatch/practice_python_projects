46b08e030e4f01def31d717c3356a59f27e11d64
nicklatch <nick.lat.73@gmail.com>
2022-02-02 17:59:09 -0600

    ..

--- character_input.py
+++ character_input.py
@@ -2,7 +2,7 @@
 def char_in():
     name = input("What is your name? ")
     age = int(input("How old are you? "))
-    year = int(input("What year is it? "))
+    year = int(input ("What year is it? "))
     bday = input("Have you had your birthday yet this year? (Y/N) ")
     if bday.upper() == 'Y':
         return(name + " you will turn 100 in the year " + str((year - age) + 100))
