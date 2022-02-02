d015d831d7ba4e317c33b1a88b27e25a5e2e331e
Nick Latcham <nick.lat.73@gmail.com>
2022-02-02 14:54:45 -0600

    Corrected birthday stipulation

--- character_input.py
+++ character_input.py
@@ -1,7 +1,10 @@
 # Character Input
-def char_in(name, age, year):
-    return(name + "you will turn 100 in the year " + str((year - age) + 100))
+def char_in(name, age, year, bday):
+    if bday.upper() == 'Y':
+        return(name + " you will turn 100 in the year " + str((year - age) + 100))
+    else:
+        return(name + " you will turn 100 in the year " + str((year - age) + 99))
 
 
 print(char_in(input("What is your name? "),
-      (int(input("How old are you? "))), 2022))
+      (int(input("How old are you? "))), int(input("What year is it? ")), input("Have you had your birthday yet this year? (Y/N)")))
