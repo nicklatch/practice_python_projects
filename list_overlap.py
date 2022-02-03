781fed78196a24976612eaa7f98875cd8a9325ef
nicklatch <nick.lat.73@gmail.com>
2022-02-03 09:01:30 -0600

    still in progress

---
+++ list_overlap.py
@@ -0,0 +1,15 @@
+# List Overlap
+import random
+
+
+def lst_overlap(l1, l2):
+    ol_lst = []
+    for num in l1:
+
+
+lst_one = [1, 2, 3, 5, 6, 7]
+lst_two = [2, 5, 7]
+
+# lst_one = random.sample(range(1, 6), random.randint(1, 6))
+# lst_two = random.sample(range(1, 6), random.randint(1, 6))
+print(lst_one, lst_two)
