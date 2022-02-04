6bf8af38dfd73c6b5ad57049d92bbe845fef17c4
Nick Latcham <nick.lat.73@gmail.com>
2022-02-04 09:53:40 -0600

    Initial program finished, still needs refinement

--- rps_game.py
+++ rps_game.py
@@ -1,4 +1,5 @@
 # RPS Game
+import sys
 
 print("ROCK, PAPER, SCISSORS")
 
@@ -12,23 +13,55 @@ p2_wins = 0
 p2_losses = 0
 p2_ties = 0
 
-# Stats Print
-print("Player 1 has " + str(p1_wins) + " Wins, " +
-      str(p1_losses) + " Losses, and " + str(p1_ties) + " Ties!")
-print("Player 2 has " + str(p2_wins) + " Wins, " +
-      str(p2_losses) + " Losses, and " + str(p2_ties) + " Ties!")
-print()
+while True:  # Main game loop
+    print("Player 1 has " + str(p1_wins) + " Wins, " +
+          str(p1_losses) + " Losses, and " + str(p1_ties) + " Ties!")
+    print("Player 2 has " + str(p2_wins) + " Wins, " +
+          str(p2_losses) + " Losses, and " + str(p2_ties) + " Ties!")
+    print()
+    while True:  # Player input loop
+        p1 = str(input(
+            "Player 1, chose your move wisely! | ROCK, PAPER, OR SCISORS: "))
+        p2 = str(input(
+            "Player 2, chose your move wisely! | ROCK, PAPER, OR SCISORS: "))
+        print()
+        if p1 == "quit" or p2 == "quit":
+            sys.exit()
+        break
 
-# Player Input
-p1 = input("Player 1, chose your move wisely! | ROCK, PAPER, OR SCISORS: ")
-p2 = input("Player 2, chose your move wisely! | ROCK, PAPER, OR SCISORS: ")
-
-while True:
+    # Scoring
     if p1 == p2:
-        p1_ties += 1
-        p2_ties += 1
         print(p1 + " vs " + p2 + "...It's a tie!")
         print()
-
-    elif p1 == "r" and p2 == "s":
+        p1_ties += 1
+        p2_ties += 1
+    elif p1 == "ROCK" and p2 == "SCISSORS":
+        print(p1 + " vs " + p2 + "...Player 1 Wins!")
+        print()
         p1_wins += 1
+        p2_losses += 1
+    elif p1 == 'PAPER' and p2 == "ROCK":
+        print(p1 + " vs " + p2 + "...Player 1 Wins!")
+        print()
+        p1_wins += 1
+        p2_losses += 1
+    elif p1 == "SCISSORS" and p2 == "PAPER":
+        print(p1 + " vs " + p2 + "...Player 1 Wins!")
+        print()
+        p1_wins += 1
+        p2_losses += 1
+    elif p2 == "ROCK" and p1 == "SCISSORS":
+        print(p1 + " vs " + p2 + "...Player 2 Wins!")
+        print()
+        p2_wins += 1
+        p1_losses += 1
+    elif p2 == 'PAPER' and p1 == "ROCK":
+        print(p1 + " vs " + p2 + "...Player 2 Wins!")
+        print()
+        p2_wins += 1
+        p1_losses += 1
+    elif p2 == "SCISSORS" and p1 == "PAPER":
+        print(p1 + " vs " + p2 + "...Player 2 Wins!")
+        print()
+        p2_wins += 1
+        p1_losses += 1
